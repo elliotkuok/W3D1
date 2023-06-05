5860e990966e865100268ca2e74d8ffde8a4a60b
Elliot Kuok <elliotckuok@gmail.com>
2023-06-05 15:13:32 -0700

    finished enummerable section

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -1,1 +1,50 @@
-#hello_world
+class Array
+    def my_each(&prc)
+        i = 0
+        while i < self.length
+            prc.call(self[i])
+            i += 1
+        end
+        self
+    end
+
+    def my_select(&prc)
+        arr = []
+        self.my_each do |ele| #if self.my_each(&prc) == true
+            if prc.call(ele) == true
+                arr << ele
+            end
+        end
+        arr
+    end
+
+    def my_reject(&prc)
+        arr = []
+        self.my_each do |ele|
+            if prc.call(ele) == false
+                arr << ele
+            end
+        end
+        arr
+    end
+
+    def my_any?(&prc)
+        self.my_each do |ele|
+            return true if prc.call(ele)  
+        end
+        false
+    end
+
+    def my_all?(&prc)
+        self.my_each do |ele|
+            return false if prc.call(ele) == false
+        end
+        true
+    end
+end
+
+a = [1, 2, 3]
+p a.my_any? { |num| num > 1 } # => true
+p a.my_any? { |num| num == 4 } # => false
+p a.my_all? { |num| num > 1 } # => false
+p a.my_all? { |num| num < 4 } # => true
