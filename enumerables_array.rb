3c34bdd1217492d0cdfa2c6d95b0c75c49fec9fd
Elliot Kuok <elliotckuok@gmail.com>
2023-06-05 17:08:37 -0700

    added my_zip method

--- enumerables_array.rb
+++ enumerables_array.rb
@@ -41,6 +41,7 @@ class Array
         end
         true
     end
+
     def my_flatten
         array = []
         self.my_each do |ele|
@@ -53,6 +54,53 @@ class Array
         array
     end
 
+    def my_zip(*args)
+        arr = []
+           
+            self.length.times do |i|
+            
+                subarr = [self[i]]
+                args.my_each do |arg|
+                    if i < arg.length
+                        subarr << arg[i]
+                    else
+                        subarr << nil
+                    end
+                end
+                arr << subarr
+            end
+        arr
+    end
+
+    
+    # def my_zip(*args)
+    #     arr = []
+        
+    #     self.my_each do |ele|
+    #         arr << [ele]
+    #     end
+        
+    #     args.each do |arg|
+    #         arg.each_with_index do |ele, i|
+    #             if arg[i].length < self.length
+    #                 arr[i] << ele
+    #             # else
+    #             #     arr[i] << nil
+    #             end
+    #         end
+    #     end
+        
+    #     arr
+    # end
 end
 
-p [1, 2, 3, [4, [5, 6]], [[[7]], 8]].my_flatten # => [1, 2, 3, 4, 5, 6, 7, 8]
+[[1], [2], [3]]
+a = [ 4, 5, 6 ]
+b = [ 7, 8, 9 ]
+p [1, 2, 3].my_zip(a, b) # => [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+p a.my_zip([1,2], [8])   # => [[4, 1, 8], [5, 2, nil], [6, nil, nil]]
+p [1, 2].my_zip(a, b)    # => [[1, 4, 7], [2, 5, 8]]
+
+c = [10, 11, 12]
+d = [13, 14, 15]
+p [1, 2].my_zip(a, b, c, d)    # => [[1, 4, 7, 10, 13], [2, 5, 8, 11, 14]]
